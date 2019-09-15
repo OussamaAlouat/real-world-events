1cb70ca402099fb71be4c3d8fda3a2422d93d108
oussamaAlouat <al317257@uji.es>
2019-09-15 13:54:15 +0200

    Add separator on links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <div id="nav" class="nav">
     <router-link class="brand" to="/">Real World Events</router-link>
     <nav>
-      <router-link :to="{ name: 'event-list' }">List</router-link>
+      <router-link :to="{ name: 'event-list' }">List</router-link> |
       <router-link :to="{ name: 'event-create' }">Create</router-link>
     </nav>
   </div>
