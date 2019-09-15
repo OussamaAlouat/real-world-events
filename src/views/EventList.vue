3bef5b289c9957f548bcae26b6fa92ee6d0f3582
oussamaAlouat <al317257@uji.es>
2019-09-15 12:14:04 +0200

    Used EventCardComponent on EventList view

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard />
     <router-link :to="{ name: 'event-show', params: {id: '1' } }">Show event</router-link>
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue';
 export default {
-
+  components: {
+    EventCard
+  }
 }
 </script>
 
