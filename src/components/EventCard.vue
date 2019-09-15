66ccfe51c1d9e2ba39000c163134a2434f5701f4
oussamaAlouat <al317257@uji.es>
2019-09-15 12:05:46 +0200

    Added EventCard component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h4>{{ title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Park cleaneup'
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>>
