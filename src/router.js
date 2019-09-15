67cba73a7d2cad75c502451c92d3c5e9b2c8db02
oussamaAlouat <al317257@uji.es>
2019-09-15 11:27:42 +0200

    Change showEvent view route, to be a dynamic route

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import EventList from "./views/EventList.vue";
 import EventCreate from "./views/EventCreate.vue";
 import EventShow from "./views/EventShow.vue";
 
-
 Vue.use(Router);
 
 export default new Router({
@@ -16,9 +15,10 @@ export default new Router({
       component: EventList
     },
     {
-      path: "/event",
+      path: "/event/:id",
       name: "event-show",
-      component: EventShow
+      component: EventShow,
+      props: true
     },
     {
       path: "/event/create",
