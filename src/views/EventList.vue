f379026c259c69ff26b333d2e33e925e81ca60dd
oussamaAlouat <al317257@uji.es>
2019-09-15 15:47:48 +0200

    Used axios to get data from fake db

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <h1>Events</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id"/>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import axios from 'axios';
+
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios.get('http://localhost:3000/events')
+      .then(response => {
+        console.log(response.data);
+        this.events = response.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 </script>
