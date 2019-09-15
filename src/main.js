9d24fc4d5d83629d17a6638244bfb7acdc16df84
oussamaAlouat <al317257@uji.es>
2019-09-15 14:46:14 +0200

    Added Global automatic registration components

--- src/main.js
+++ src/main.js
@@ -4,6 +4,28 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+// Global components.
+
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+const requireComponent = require.context(
+  // The relative path of the components folder
+  "./components",
+  // Whether or not to look in subfolders
+  false,
+  // The regular expression used to match base component filenames
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 new Vue({
   router,
