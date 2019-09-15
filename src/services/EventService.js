b678dcb402ea03c4c61e4fa7a049d69d155f3712
oussamaAlouat <al317257@uji.es>
2019-09-15 16:17:31 +0200

    Extract axios method on a service file

---
+++ src/services/EventService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-type": "application/json"
+  }
+});
+
+export default {
+  getEvents() {
+    return apiClient.get("/events");
+  }
+}
