7e621ef503d1fd6bc03c6c3102f95535970105f4
oussamaAlouat <al317257@uji.es>
2019-09-15 16:08:17 +0200

    Called to EventCard with event prop

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Events</h1>
-    <EventCard v-for="event in events" :key="event.id"/>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
